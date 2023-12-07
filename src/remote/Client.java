20b6bd615100b0c280e40e647e4eecd33a9b0884
Ruisth23 <rmfde@iscte-iul.pt>
2023-12-07 14:50:39 +0000

    Nova branch (server diferente)

--- src/remote/Client.java
+++ src/remote/Client.java
@@ -1,90 +1,16 @@
 package remote;
 
 
-import environment.Board;
-import game.Server;
-import game.Snake;
-import gui.SnakeGui;
-
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 /** Remore client, only for part II
- * 
+ *
  * @author luismota
  *
  */
 
 public class Client {
 
-	private ObjectInputStream input;
-	private PrintWriter output;
-	private Socket socket;
-	private RemoteBoard snakeGui;
-
-	public Client(String nickName) {
-		this.snakeGui = new RemoteBoard(nickName);
-	}
-
-	public void runClient(String address, int port) {
-		try {
-			snakeGui.init();
-			connect(address, port);
-			while (true) {
-				receive();
-				send();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}catch (ClassNotFoundException | ClassCastException e2){
-			return;
-        } finally {
-			try {
-				if (socket != null) {
-					socket.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-    }
-
-	void send() throws IOException {
-		String directionPressed = snakeGui.getPressedDirection();
-		if (directionPressed != null) {
-			output.println(directionPressed);
-		}
-	}
-
-	void receive() throws IOException, ClassNotFoundException {
-		LinkedList<Snake> read = (LinkedList<Snake>) input.readObject();
-		snakeGui.getBoard().setSnakes(read);
-	}
-
-	void connect(String address, int port) throws IOException {
-		if (address == "") {
-			InetAddress adr = InetAddress.getByName(null);
-			socket = new Socket(adr, Server.PORT);
-		} else {
-			socket = new Socket(address, port);
-		}
-		input = new ObjectInputStream(socket.getInputStream());
-		output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-	}
-
 	public static void main(String[] args) {
-		String serverAddress = "localhost";
-		int serverPort = 25565;
-
-		new Client("Player 1").runClient(serverAddress,serverPort);
+		// TODO
 	}
 
 }
--- src/remote/RemoteBoard.java
+++ src/remote/RemoteBoard.java
@@ -1,6 +1,5 @@
 package remote;
 
-import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.PrintWriter;
@@ -13,67 +12,18 @@ import environment.Cell;
 import game.Goal;
 import game.Obstacle;
 import game.Snake;
-import gui.SnakeGui;
-import javax.swing.JFrame;
-
-import static java.awt.event.KeyEvent.*;
 
 /** Remote representation of the game, no local threads involved.
- * Game state will be changed when updated info is received from Server.
+ * Game state will be changed when updated info is received from Srver.
  * Only for part II of the project.
  * @author luismota
  *
  */
 public class RemoteBoard extends Board{
 
-	private boolean wasdOrArrows;
-	private String directionPressed;
-	private JFrame frame;
-	private SnakeGui snakeGui;
-	public Board board;
-	public RemoteBoard(String frameName) {
-		super();
-		frame = new JFrame(frameName);
-		snakeGui = new SnakeGui(frameName);
-	}
-
-	public Board getBoard() {
-		return board;
-	}
-
 	@Override
 	public void handleKeyPress(int keyCode) {
-		if (wasdOrArrows) {
-			switch (keyCode) {
-				case VK_A:
-					directionPressed = "LEFT";
-					break;
-				case VK_D:
-					directionPressed = "RIGHT";
-					break;
-				case VK_W:
-					directionPressed = "UP";
-					break;
-				case VK_S:
-					directionPressed = "DOWN";
-					break;
-			}
-		} else {
-			switch (keyCode) {
-				case VK_LEFT:
-					directionPressed = "LEFT";
-					break;
-				case VK_RIGHT:
-					directionPressed = "RIGHT";
-					break;
-				case VK_UP:
-					directionPressed = "UP";
-					break;
-				case VK_DOWN:
-					directionPressed = "DOWN";
-					break;
-			}
-		}
+		//TODO
 	}
 
 	@Override
@@ -84,12 +34,9 @@ public class RemoteBoard extends Board{
 	@Override
 	public void init() {
 		// TODO
-		frame.setVisible(true);
 	}
 
-	public String getPressedDirection() {
-		return directionPressed;
-	}
+
 
 
 }
